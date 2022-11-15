d506a251d8ac57f8bbe88b9fd2d8961808f045ea
thuanghai <t_huanghai@qq.com>
2022-11-15 19:01:28 +0800

    fix bug for bash

--- efak/entrypoint.sh
+++ efak/entrypoint.sh
@@ -1,4 +1,3 @@
-#!/usr/bin/env bash
-/opt/efak/bin/ke.sh start
+sh /opt/efak/bin/ke.sh start
 tail -100f /opt/efak/kms/logs/catalina.out
 #tail -100f /opt/efak/logs/log.log
--- efak/healthcheck.sh
+++ efak/healthcheck.sh
@@ -1,4 +1,3 @@
-#!/usr/bin/env bash
 if [ `jps |grep KafkaEagle | wc -l` != 1 ]; then
   ps -ef | grep tail | head -1 |awk '{print $2}' | xargs -I {} kill -9 {}
   exit 1
