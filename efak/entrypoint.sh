0eed17d7a95d1d4c9dd8d815ed755d2e06218e8a
thuanghai <t_huanghai@qq.com>
2022-11-15 17:55:09 +0800

    update 3.0.2

--- efak/entrypoint.sh
+++ efak/entrypoint.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
-sh /opt/efak/bin/ke.sh start
-tail -f /opt/efak/kms/logs/catalina.out
+/opt/efak/bin/ke.sh start
+tail -100f /opt/efak/kms/logs/catalina.out
+#tail -100f /opt/efak/logs/log.log
